8c733fef38a0d64367ae0a4ae405ed18996d6695
Jeremy Robson <jeremy.robson@gmail.com>
2014-11-05 19:51:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,31 @@ var QUAD_SIZE = 64;
 var MAP_WIDTH = 1024;
 var MAP_HEIGHT = 1024;
 
+//try moving in three directions according to pinwheel navigation
+var get_vector = function(map, x, y, direction) {
+    var v = pathtable[direction];
+    var m = {"x":wrap(x+v[0],map.width),"y":wrap(y+v[1],map.height)};
+    
+    var r = Math.random() < 0.5 ? -1 : 1; //random sign to prevent getting stuck
+    var rd = [r,-r];
+    
+    if (map.blocked(m.x,m.y)) {
+        v = pathtable[(direction+rd[0])&7];
+        m = {"x":wrap(x+v[0],map.width),"y":wrap(y+v[1],map.height)};
+    }
+    
+    if (map.blocked(m.x,m.y)) {
+        v = pathtable[(direction+rd[1])&7];
+        m = {"x":wrap(x+v[0],map.width),"y":wrap(y+v[1],map.height)};
+    }
+    
+    if (map.blocked(m.x,m.y)) v = [0,0];
+    return v;
+};
+
 function create_map(width, height) {
+    var id = randint(1000,100000);
+    var unitindices = [];
     var tile = [];
     for (var x=0;x<width;x++) {
         tile[x] = [];
@@ -33,305 +57,215 @@ function create_map(width, height) {
             tile[x][y] = pick_random([tiletemplates[0],tiletemplates[1], tiletemplates[1]]);
         }
     }
-    var quad = [];
-    for (var x=0;x<width/QUAD_SIZE;x++) {
-        quad[x] = [];
-        for (var y=0;y<height/QUAD_SIZE;y++) {
-            quad[x][y] = [];
-        }
-    }
-    
-    var get_quad = function(x, y, min, max) {
-        var qx = Math.floor(x / QUAD_SIZE);
-        var qy = Math.floor(y / QUAD_SIZE);
-        var unitindices = [];
-        for (var i=min;i<=max;i++) {
-            for (var j=min;j<=max;j++) {
-                unitindices = unitindices.concat(quad[wrap(qx+i,16)][wrap(qy+j,16)]);
-            }
-        }
-        return unitindices;
-    };
 
-    var npc_for_each = function(fn) {
-        for (var x=0;x<width/QUAD_SIZE;x++) {
-            for (var y=0;y<height/QUAD_SIZE;y++) {
-                quad[x][y].forEach(fn);
-            }
-        }
+    /*
+    for (var i=0;i<3;i++) {
+        var randx = randint(0, width);
+        var randy = randint(0, height);
+        tile[randx][randy].door = {"submap": create_map(randint(64, 256), randint(64, 256)) };
+    }
+    */
+    
+    var add = function(index) {
+        unitindices.push(index);
     };
     
-    var get_vector = function(x, y, direction) {
-        //try moving in three directions according to pinwheel navigation
-        var v = pathtable[direction];
-        var m = {"x":wrap(x+v[0],width),"y":wrap(y+v[1],height)};
-        if (tile[m.x][m.y].blocked) {
-            v = pathtable[(direction+1)&7];
-            m = {"x":wrap(x+v[0],width),"y":wrap(y+v[1],height)};
-        }
-        if (tile[m.x][m.y].blocked) {
-            v = pathtable[(direction-1)&7];
-            m = {"x":wrap(x+v[0],width),"y":wrap(y+v[1],height)};
-        }
-        if (tile[m.x][m.y].blocked) v = [0,0];
-        return v;
+    var remove = function(index) {
+        unitindices = unitindices.filter(function(i) { return i != index; });
     };
     
-    var add = function(index, qx, qy) {
-        quad[qx][qy].push(index);
-    };
-    
-    var remove = function(index, qx, qy) {
-        quad[qx][qy] = quad[qx][qy].filter(function(a) {
-            return a != index;
+    var get_units = function(exclude) {
+        return unitindices.filter(function(index) {
+            return index != exclude;
         });
     };
     
     return {
         add: add,
         remove: remove,
-        get_vector: get_vector,
         width: width,
         height: height,
         tile: tile,
-        get_quad: get_quad,
-        npc_for_each: npc_for_each
+        blocked: function(x, y) { return tile[x][y].blocked; },
+        get_units: get_units,
+        get_id: function() { return id; }
     };
 }
 
-function create_npc(ai, x, y) {
+function to_string(u) {
+    return u.NAME + ": " + u.get_x + ", " + u.get_y;
+};
+
+function create_unit(ai, NAME, color, map, x, y) {
     var index = -1;
     var id = randint(10000, 1000000);
-    var NAME = "NPC";
-    var mapx = x || randint(0, MAP_WIDTH);
-    var mapy = y || randint(0, MAP_HEIGHT);
-    var quadx = Math.floor(mapx / QUAD_SIZE);
-    var quady = Math.floor(mapy / QUAD_SIZE);
-    var oldquadx = -1;
-    var oldquady = -1;
-    var color = "rgb(255,0,0)";
+    var NAME = NAME || "NPC";
+    var mapx = x || randint(0, map.width);
+    var mapy = y || randint(0, map.height);
+    var color = color || "rgb(255,0,0)";
     var hp = 100;
     var agl = randint(3,20);
     var at = 0;  //todo: change at to tickcount, less calculations
     var target = null;
     
-    var move = function(map, dx, dy) {
-        mapx = wrap(mapx + dx, MAP_WIDTH);
-        mapy = wrap(mapy + dy, MAP_HEIGHT);
+    function move(dx, dy) {
+        var x = wrap(mapx + dx, map.width);
+        var y = wrap(mapy + dy, map.height);
         
-        var newquadx = Math.floor(mapx / QUAD_SIZE);
-        var newquady = Math.floor(mapy / QUAD_SIZE);
-        
-        if (newquadx != quadx || newquady != quady) {
-            oldquadx = quadx;
-            oldquady = quady;
-            quadx = newquadx;
-            quady = newquady;
-            map.add(index, newquadx, newquady);
-            map.remove(index, oldquadx, oldquady);
-        }
-    };
+        if (map.blocked(x, y))
+            console.log("error", x, y, dx, dy);
+            
+        mapx = x;
+        mapy = y;
+    }
     
-    var turn = function(units, map) {
-        at = at + agl;
-        if (at >= 1000) {
-            /** possible bottleneck!! **/
-        
-            var distances = units.filter(function(u) {
-                return u.get_ai() != ai;
-            }).map(function(u) {
-                var x = u.get_x() - mapx;
-                var y = u.get_y() - mapy;
-                var distance = Math.sqrt(x * x + y * y);
-                return {unit:u, distance:distance};
-            });
-            
-            var closest = distances.sort(function(a, b) {
-                return a.distance - b.distance;
-            })[0];
-            
-            if (closest) target = closest.unit;
-            
-            var xmove = randint(-1,2);
-            var ymove = randint(-1,2);
-            
-            if (target) {
-                var targetx = target.get_x();
-                var targety = target.get_y();
-                var angle = Math.atan2(mapy - targety, mapx - targetx);
-                var direction = (Math.floor((angle*4+Math.PI/2)/Math.PI))&7;  //pinwheel navigation
-                var vector = map.get_vector(mapx, mapy, direction);
-                xmove = vector[0];
-                ymove = vector[1];
-                if (Math.abs(targetx - mapx) > MAP_WIDTH / 2) xmove = -xmove;
-                if (Math.abs(targety - mapy) > MAP_HEIGHT / 2) ymove = -ymove;
-            }
-            
-            move(map, xmove, ymove);
+    function turn(units, map) {
+        at += agl;
+        if (at >= 500) {
+            if (ai == "hostile")
+                npc_turn(units);
+            else
+                if (target) invoke_action();
             at = 0;
         }
-    };
+    }
     
-    var to_string = function() {
-        return NAME + ": " + mapx + ", " + mapy;
-    };
+    function npc_turn(units) {
+        /** possible bottleneck!! **/
+
+        var distances = units.filter(function(u) {
+            return u.get_ai() != ai;
+        }).map(function(u) {
+            var x = u.get_x() - mapx;
+            var y = u.get_y() - mapy;
+            var distance = Math.sqrt(x * x + y * y);
+            return {unit:u, distance:distance};
+        });
+        
+        var closest = distances.sort(function(a, b) {
+            return a.distance - b.distance;
+        })[0];
+        
+        if (closest) target = closest.unit;
+        
+        var xmove = randint(-1,2);
+        var ymove = randint(-1,2);
+        
+        if (target) {
+            var targetx = target.get_x();
+            var targety = target.get_y();
+            var angle = Math.atan2(mapy - targety, mapx - targetx);
+            var direction = (Math.floor((angle*4+Math.PI/2)/Math.PI))&7;  //pinwheel navigation
+            if ((Math.abs(targetx - mapx) > map.width / 2) || (Math.abs(targety - mapy) > map.height / 2))
+                direction = (direction + 4) & 7; //reverse direction accross wrap
+            var vector = get_vector(map, mapx, mapy, direction);
+            xmove = vector[0];
+            ymove = vector[1];
+        }
+        
+        move(xmove, ymove);
+    }
+    
+    function invoke_action() {
+        var action = {
+            "actor": index,
+            "target": target.get_index(),
+            "x1": mapx,
+            "y1": mapy,
+            "x2": target.get_x(),
+            "y2": target.get_y(),
+            "type": "attack"
+        };
+    }
     
     return {
         get_index: function() { return index; },
-        set_index: function(i) { index = i; },
+        set_index: function(i) { index = i; map.add(index); },
         get_ai: function() { return ai; },
         color: color,
+        get_target: function() { return target; },
+        set_target: function(u) { target = u; console.log(target); },
+        get_position: function() { return [mapx, mapy]; },
         get_x: function() { return mapx; },
         get_y: function() { return mapy; },
-        get_quad_x: function() { return Math.floor(mapx / QUAD_SIZE); },
-        get_quad_y: function() { return Math.floor(mapy / QUAD_SIZE); },
+        set_x: function(x) { mapx = x; },
+        set_y: function(y) { mapy = y; },
+        get_unit_data: function() { return get_unit_data(index); },
         move: move,
         turn: turn,
-        to_string: to_string
+        to_json: function() { return {"index":index,"NAME":NAME,"color":color,"x":mapx,"y":mapy}; },
+        to_string: function() { return to_string(this); }
     };
 }
 
-function create_user(client) {
-    var index = -1;
-    var id = randint(10000, 1000000);
-    var ai = "user";
-    var NAME = "User";
-    var mapx = 0;
-    var mapy = 0;
-    var quadx = Math.floor(mapx / QUAD_SIZE);
-    var quady = Math.floor(mapy / QUAD_SIZE);
-    var oldquadx = -1;
-    var oldquady = -1;
-    var color = "rgb(0,255,255)";
-    var hp = 100;
-    var agl = randint(3,20);
-    var at = 0;
-    var target = null;
+function create_user(client, map) {
+    var username = "jeremy";
+    var unit = create_unit("user", "User1", "rgb(0,255,255)", map, 1, 1);
     
     var send_message = function(type, data) {
         client.send({"type":type, "data":data}); //use JSON.stringify(data)
-    };
-
-    var move = function(map, dx, dy) {
-        mapx = wrap(mapx + dx, MAP_WIDTH);
-        mapy = wrap(mapy + dy, MAP_HEIGHT);
-        var newquadx = Math.floor(mapx / QUAD_SIZE);
-        var newquady = Math.floor(mapy / QUAD_SIZE);
-        
-        if (newquadx != quadx || newquady != quady) {
-            oldquadx = quadx;
-            oldquady = quady;
-            quadx = newquadx;
-            quady = newquady;
-            map.add(index, newquadx, newquady);
-            map.remove(index, oldquadx, oldquady);
-        } 
     };
     
     var invoke_action = function(targetindex) {
         send_message("action", targetindex);
     };
     
-    var turn = function(units, map) {
-        at = at + agl;
-        if (at >= 1000) {
-            if (target) {
-                invoke_action({
-                    "actor": index,
-                    "target": target.get_index(),
-                    "x1": mapx,
-                    "y1": mapy,
-                    "x2": target.get_x(),
-                    "y2": target.get_y(),
-                    "type": "attack"
-                });
-            }
-            
-            at = 0;
-        }
-    };
-    
-    var update_units = function(unitdata) {
-        send_message("unitupdate", unitdata);
+    var update_units = function(units) {
+        var unitlist = map.get_units(unit.get_index());
+        var data = unitlist.map(function(index) { return units[index].to_json(); });
+        send_message("unitupdate", data);
+        var target = unit.get_target();
         if (target) send_message("target", target.get_index());
     };
     
-    var to_string = function() {
-        return NAME + ": " + mapx + ", " + mapy;
+    var move = function(direction) {
+        var vector = get_vector(map, unit.get_x(), unit.get_y(), direction);
+        unit.move(vector[0], vector[1]);
     };
     
     return {
-        get_index: function() { return index; },
-        set_index: function(i) { index = i; },
-        NAME: NAME,
-        color: color,
-        get_ai: function() { return ai; },
-        move: move,
-        get_x: function() { return mapx; },
-        get_y: function() { return mapy; },
         client: client,
         send_message: send_message,
-        set_target: function(u) { target = u; },
-        turn: turn,
+        set_target: function(u) { unit.set_target(u); },
         update_units: update_units,
-        to_string: to_string
-    }
+        to_string: function() { return to_string(this); },
+        get_unit: function() { return unit; },
+        set_map: function(newmap) { map = newmap; },
+        get_map: function() { return map; },
+        get_units: function() { return map.get_units(this); },
+        move: move
+    };
 }
 
 function create_server(client) {
-    var map = create_map(MAP_WIDTH, MAP_HEIGHT);
+    var map = create_map(256, 256);
     var units = [];
     var user;
 
     var add = function(unit) {
         unit.set_index(units.push(unit) - 1);
-        unit.move(map, unit.get_x(), unit.get_y());
     };
     
     var loop = function() {
         if (user) {
-            user.send_message("move", [user.get_x(), user.get_y()]);
-            update_units(user);
+            user.send_message("move", user.get_unit().get_position());
+            user.update_units(units);
         }
         
-        units.filter(function(u) {
+        units.forEach(function(u) {
             u.turn(units, map);
         });
     };
     
-    var update_units = function(unit) {
-        //limit to only when unit's quad changes
-        //if (unit.quadchanged) {
-        //var unitlist = map.get_quad(unit.get_x(), unit.get_y(), -1, 1).map(function(i) {
-        //    return units[i];
-        //});
-        //.filter(function(u) {
-        //    return u.get_index() != unit.get_index();
-        //});;
-        var unitdata = [];
-        unitdata = units.map(function(u) {
-            return {
-                "index": u.get_index(),
-                "NAME": u.name,
-                "color": u.color,
-                "x": u.get_x(),
-                "y": u.get_y()
-            };
-        });
-        unit.update_units(unitdata);
-        //}
-    };
-    
     var on_message = function(message) { //todo: arg also contains which user
         if (message.type == "connect") {
-            user = create_user(message.data);
-            add(user);
+            user = create_user(message.data, map);
+            add(user.get_unit());
             user.send_message("sysmessage", "connection successful!");
         }
     
         if (message.type == "move") {
-            var vector = map.get_vector(user.get_x(), user.get_y(), message.data);
-            user.move(map, vector[0], vector[1]);
+            user.move(message.data);
         }
         
         if (message.type == "target") {
@@ -341,14 +275,12 @@ function create_server(client) {
     
     /*
     for (var i=0;i<200;i++) {
-        var npc = create_npc("hostile");
-        var qx = npc.get_quad_x();
-        var qy = npc.get_quad_y();   
-        npc.set_index(units.push(npc) - 1);
+        var npc = create_unit("hostile", "NPC", "rgb(255,0,0)", map, randint(0,256), randint(0,256));
+        add(npc);
     }
     */
     
-    var npc = create_npc("hostile", 5, 5);
+    var npc = create_unit("hostile", "NPC", "rgb(255,0,0)", map, 5, 5);
     add(npc);
     
     return {
